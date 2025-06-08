7a97abb9dc1c9a6cc725610bcc97b7e4a95cd82a
wouter <wouter@debruijn.dev>
2025-06-08 10:12:54 +0200

    feat: add vite react-router

---
+++ apps/web/app/components/chat.tsx
@@ -0,0 +1,14 @@
+export function Chat() {
+  return (
+    <div className="text-primary">
+      Dolore mollit aliqua ad culpa. Commodo est id cillum. Tempor minim
+      pariatur ullamco exercitation occaecat. Aute eiusmod commodo magna culpa
+      ex aliqua nisi aute non. Sint ex minim dolor esse pariatur duis et
+      reprehenderit nisi. Cillum dolor commodo aliqua consequat. Cupidatat esse
+      eiusmod elit. Est voluptate ut deserunt labore irure dolor. Quis cillum
+      minim adipisicing ipsum ad sunt non anim. Lorem quis voluptate non.
+      Exercitation consequat amet commodo veniam officia veniam non elit eu sunt
+      ex anim ex sint amet. Irure laborum eiusmod elit deserunt nulla.
+    </div>
+  );
+}
---
+++ apps/web/app/routes/home.tsx
@@ -0,0 +1,13 @@
+import { Chat } from "~/components/chat";
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <Chat />;
+}
