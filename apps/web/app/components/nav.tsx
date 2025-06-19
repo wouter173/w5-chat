bde1c21502d2739d749e23556a228eb7c88cea62
wouter <wouter@debruijn.dev>
2025-06-19 21:01:23 +0200

    feat: remove super from super link

--- apps/web/app/components/nav.tsx
+++ apps/web/app/components/nav.tsx
@@ -28,95 +28,90 @@ export function Nav() {
         <header className="pb-4 pt-2">
           <h1 className="w-full text-center text-xl font-semibold ">W5 Chat</h1>
         </header>
-        <SignedIn>
-          <Link
-            onMouseDown={() => setNewButtonActive(true)}
-            onMouseUp={() => setNewButtonActive(false)}
-            onMouseLeave={() => setNewButtonActive(false)}
-            to="/"
-            className={cn(
-              'text-primary bg-zinc-800 transition-all text-sm py-1.5 px-2 rounded-lg text-center shadow-[0_-1px_rgba(255,255,255,0.25),0_4px_8px_rgba(0,0,0,0.05),0_1px_6px_-4px_#000]',
-              newButtonActive ? 'scale-[97%]' : 'scale-100',
-            )}
-            viewTransition
-          >
-            New Chat
-          </Link>
-        </SignedIn>
+
+        <Link
+          onMouseDown={() => setNewButtonActive(true)}
+          onMouseUp={() => setNewButtonActive(false)}
+          onMouseLeave={() => setNewButtonActive(false)}
+          to="/"
+          className={cn(
+            'text-primary bg-zinc-800 transition-all text-sm py-1.5 px-2 rounded-lg text-center shadow-[0_-1px_rgba(255,255,255,0.25),0_4px_8px_rgba(0,0,0,0.05),0_1px_6px_-4px_#000]',
+            newButtonActive ? 'scale-[97%]' : 'scale-100',
+          )}
+          viewTransition
+        >
+          New Chat
+        </Link>
       </div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <div className="relative h-full flex-1 overflow-hidden">
-          <div className="pointer-events-none absolute top-0 left-0 right-px h-6 bg-gradient-to-b from-background via-background/80 z-30"></div>
-          <div className="pointer-events-none absolute bottom-0 left-0 right-px h-6 bg-gradient-to-t from-background via-background/80 z-30"></div>
-          <ul className="h-full overflow-y-scroll overflow-x-hidden px-2.5 no-scrollbar py-[13px] ">
-            <AnimatePresence>
-              {listQuery.data?.map((chat) => (
-                <motion.li
-                  initial={{ opacity: 0 }}
-                  transition={{ duration: 0.15 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  key={chat.id}
+
+      <div className="relative h-full flex-1 overflow-hidden">
+        <div className="pointer-events-none absolute top-0 left-0 right-px h-6 bg-gradient-to-b from-background via-background/80 z-30"></div>
+        <div className="pointer-events-none absolute bottom-0 left-0 right-px h-6 bg-gradient-to-t from-background via-background/80 z-30"></div>
+        <ul className="h-full overflow-y-scroll overflow-x-hidden px-2.5 no-scrollbar py-[13px] ">
+          <AnimatePresence>
+            {listQuery.data?.map((chat) => (
+              <motion.li
+                initial={{ opacity: 0 }}
+                transition={{ duration: 0.15 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                key={chat.id}
+                className={cn(
+                  'group relative z-20 transition-colors duration-100 border border-transparent rounded-lg text-zinc-400 flex items-center justify-between',
+                  'hover:text-zinc-200',
+                  params.id === chat.id && 'sticky top-0 bottom-0 z-30 bg-panel text-zinc-50 border-zinc-800 ',
+                )}
+              >
+                <SuperLink to={`/${chat.id}`} className="truncate py-1.5 text-sm px-2.5 w-full" title={chat.name}>
+                  {chat.name}
+                </SuperLink>
+                <button
+                  onClick={async () => {
+                    await deleteMutation.mutateAsync({ chatId: chat.id })
+                    await listQuery.refetch()
+                  }}
                   className={cn(
-                    'group relative z-20 transition-colors duration-100 border border-transparent rounded-lg text-zinc-400 flex items-center justify-between',
-                    'hover:text-zinc-200',
-                    params.id === chat.id && 'sticky top-0 bottom-0 z-30 bg-panel text-zinc-50 border-zinc-800 ',
+                    'opacity-0 group-hover:opacity-50 transition-all mr-1 cursor-pointer hover:opacity-100 p-1 block rounded-sm duration-75',
+                    'active:scale-95',
                   )}
                 >
-                  <SuperLink to={`/${chat.id}`} className="truncate py-1.5 text-sm px-2.5 w-full" title={chat.name}>
-                    {chat.name}
-                  </SuperLink>
-                  <button
-                    onClick={async () => {
-                      await deleteMutation.mutateAsync({ chatId: chat.id })
-                      await listQuery.refetch()
-                    }}
-                    className={cn(
-                      'opacity-0 group-hover:opacity-50 transition-all mr-1 cursor-pointer hover:opacity-100 p-1 block rounded-sm duration-75',
-                      'active:scale-95',
-                    )}
-                  >
-                    <Trash2Icon size={16} />
-                  </button>
-                </motion.li>
-              ))}
-            </AnimatePresence>
-          </ul>
-        </div>
-        <div className="p-2 pb-4 border-t border-zinc-800">
-          <div className="group h-13 flex gap-2">
-            <button
-              className="flex items-center gap-2 text-left w-full px-3 py-2 hover:bg-zinc-900 rounded-xl cursor-pointer"
-              onClick={() => clerk.openUserProfile()}
-            >
-              <div className="w-7 h-7">
-                <img src={user?.imageUrl} className="rounded-full" />
-              </div>
+                  <Trash2Icon size={16} />
+                </button>
+              </motion.li>
+            ))}
+          </AnimatePresence>
+        </ul>
+      </div>
+      <div className="p-2 pb-4 border-t border-zinc-800">
+        <div className="group h-13 flex gap-2">
+          <button
+            className="flex items-center gap-2 text-left w-full px-3 py-2 hover:bg-zinc-900 rounded-xl cursor-pointer"
+            onClick={() => clerk.openUserProfile()}
+          >
+            <div className="w-7 h-7">
+              <img src={user?.imageUrl} className="rounded-full" />
+            </div>
 
-              <div className="flex flex-col text-sm text-zinc-100">
-                <span>{user?.fullName ?? user?.primaryEmailAddress?.emailAddress}</span>
-                <span className="text-xs text-zinc-500">{user?.primaryEmailAddress?.emailAddress}</span>
-              </div>
-            </button>
-            <button
-              onClick={async (e) => {
-                e.stopPropagation()
-                await clerk.signOut()
-                navigate('/sign-in', { replace: true })
-              }}
-              className={cn(
-                'ml-auto opacity-0 group-hover:opacity-50 transition-all mr-1 cursor-pointer hover:opacity-100 p-1 block rounded-sm duration-75',
-                'active:scale-95',
-              )}
-            >
-              <LogOutIcon size={16} />
-            </button>
-          </div>
+            <div className="flex flex-col text-sm text-zinc-100">
+              <span>{user?.fullName ?? user?.primaryEmailAddress?.emailAddress}</span>
+              <span className="text-xs text-zinc-500">{user?.primaryEmailAddress?.emailAddress}</span>
+            </div>
+          </button>
+          <button
+            onClick={async (e) => {
+              e.stopPropagation()
+              await clerk.signOut()
+              navigate('/sign-in', { replace: true })
+            }}
+            className={cn(
+              'ml-auto opacity-0 group-hover:opacity-50 transition-all mr-1 cursor-pointer hover:opacity-100 p-1 block rounded-sm duration-75',
+              'active:scale-95',
+            )}
+          >
+            <LogOutIcon size={16} />
+          </button>
         </div>
-      </SignedIn>
+      </div>
     </nav>
   )
 }
--- apps/web/app/components/super-link.tsx
+++ apps/web/app/components/super-link.tsx
@@ -2,9 +2,5 @@ import type { ComponentProps } from 'react'
 import { Link } from 'react-router'
 
 export function SuperLink(props: ComponentProps<typeof Link>) {
-  return (
-    <Link {...props} onMouseDown={(e) => e.currentTarget.click()}>
-      {props.children}
-    </Link>
-  )
+  return <Link {...props}>{props.children}</Link>
 }
